1579a180f1361f148bd26a51c3a3271710e7ee68
Zeke Boggs <94147496+BigBoggle@users.noreply.github.com>
2026-04-12 15:53:55 -0400

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+
+       std::unordered_map<int, int> data;
+       for(int i = 0; i < nums.size(); i++){
+            data[nums[i]] = i;
+       } 
+
+       for(int i = 0; i < nums.size(); i++){
+           int diff =  target - nums[i];
+
+           if(data.count(diff) && data[diff] != i){
+                return {i, data[diff]}; 
+           }
+
+       }
+       return {};
+    }
+};
